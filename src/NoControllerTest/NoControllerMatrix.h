4ea3b1260b2e57d35afe9b731c314fbc7c2be0a1
Damian <damianpeaf@gmail.com>
2023-02-08 22:55:40 -0600

    feat: No controller matrix lib

--- src/NoControllerTest/NoControllerMatrix.h
+++ src/NoControllerTest/NoControllerMatrix.h
@@ -9,19 +9,24 @@ class NoControllerMatrix {
     // TODO: Use a cpp file for this class  
 
   public:
-    NoControllerMatrix(int rowPins[8], int columnPins[8]);
+    NoControllerMatrix(int rp[8], int cp[8]) {
+        for(int i = 0; i < 8; i++){
+            this->rowPins[i] = rp[i];
+            this->columnPins[i] = cp[i];
+        }
+    }
     
     void setMatrix(bool matrix[8][8]){
         for(int row = 0; row < 8; row++){
 
             // Turn on the row
-            digitalWrite(rowPins[row], LOW);
+            digitalWrite(this->rowPins[row], LOW);
 
             // Turn on the columns
             for(int column = 0; column < 8; column++){
                 
                 if(matrix[row][column]){
-                    digitalWrite(columnPins[column], HIGH);
+                    digitalWrite(this->columnPins[column], HIGH);
                 }
             }
 
@@ -29,32 +34,34 @@ class NoControllerMatrix {
             delay(MULTIPLEXER_DELAY);
 
             // Turn off the row
-            digitalWrite(rowPins[row], HIGH);
+            digitalWrite(this->rowPins[row], HIGH);
 
             // Turn off the columns
             for(int column = 0; column < 8; column++){
-                digitalWrite(columnPins[column], LOW);
+                digitalWrite(this->columnPins[column], LOW);
             }
         }
     };
 
     void initMatrix(){
         for(int pin = 0; pin < 8; pin++){
-            pinMode(rowPins[row], OUTPUT);
-            pinMode(columnPins[row], OUTPUT);
+            pinMode(this->rowPins[pin], OUTPUT);
+            pinMode(this->columnPins[pin], OUTPUT);
         }
     }
 
     void resetMatrix(){
         for(int pin = 0; pin < 8; pin++){
-            digitalWrite(rowPins[row], LOW);
-            digitalWrite(columnPins[row], HIGH);
+            digitalWrite(this->rowPins[pin], LOW);
+            digitalWrite(this->columnPins[pin], HIGH);
         }
     }
     
 
   private:
     int MULTIPLEXER_DELAY = 1;
+    int rowPins[8];
+    int columnPins[8];
 };
 
 #endif
