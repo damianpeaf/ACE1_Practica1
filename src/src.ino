b6443c05935272cf90148a1ab744eb39f992408c
Damian <damianpeaf@gmail.com>
2023-02-09 08:10:38 -0600

    refactor: NoControllerMatrix lib

---
+++ src/src.ino
@@ -0,0 +1,19 @@
+/*
+    Código fuente - Práctica 1 - Arquitectura de Computadores y Ensambladores 1
+    Grupo:  07
+    Integrantes:
+        - Alvaro Norberto García Meza 202109567
+        - Damián Ignacio Peña Afre 202110568
+        - Daniel Estuardo Cuque Ruíz 202112145
+        - José Manuel Ibarra Pirir 202001800
+*/
+
+#include "NoControllerMatrix/NoControllerMatrix.h"
+
+void setup() {
+    // put your setup code here, to run once:
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
